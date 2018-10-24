3b7cd4beeaae7be9b0a68c1248a4bdfaf0a50b76
Matthew R Becker <becker.mr@gmail.com>
2018-10-24 13:36:19 -0700

    setup the repo

---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+scripts = ['bin/pizza-cutter']
+
+__version__ = None
+pth = os.path.join(
+    os.path.dirname(os.path.realpath(__file__)),
+    "pizza_cutter",
+    "_version.py")
+with open(pth, 'r') as fp:
+    exec(fp.read())
+
+setup(
+    name='pizza_cutter',
+    version=__version__,
+    description="yummy survey slices",
+    author="MRB and ESS",
+    packages=find_packages(),
+    include_package_data=True,
+    scripts=scripts,
+)
