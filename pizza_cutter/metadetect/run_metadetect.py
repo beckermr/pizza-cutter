5450a2ccf393a671b582cda0e2959aae78fcded3
Matthew R Becker <becker.mr@gmail.com>
2018-11-02 16:33:24 -0500

    add new scripts for running stuff

---
+++ pizza_cutter/metadetect/run_metadetect.py
@@ -0,0 +1,20 @@
+
+
+def run_metadetect(
+        *,
+        config,
+        meds_file_list,
+        output_path):
+    """Run metadetect on a "pizza slice" MEDS file and write the outputs to
+    disk.
+
+    Parameters
+    ----------
+    config : dict
+        The metadetect configuration file.
+    meds_file_list : list of str
+        A list of the input MEDS files to run on.
+    output_path : str
+        The path to which to write the outputs.
+    """
+    pass
