fdb9e0bd74c784f13c916e3c3b9f400e1c0637c7
Matthew R Becker <becker.mr@gmail.com>
2018-11-10 09:30:46 -0600

    TST tests of interpolation

--- pizza_cutter/des_pizza_cutter/tests/test_slice_data.py
+++ pizza_cutter/des_pizza_cutter/tests/test_slice_data.py
@@ -4,7 +4,8 @@ import pytest
 from .._slice_data import (
     symmetrize_bmask,
     symmetrize_weight,
-    build_slice_locations)
+    build_slice_locations,
+    interpolate_image_and_noise)
 
 
 def test_build_slice_locations():
@@ -48,3 +49,66 @@ def test_symmetrize_bmask():
          [1, 0, 2, 0],
          [1, 0, 2, 0],
          [1, 1, 3, 1]])
+
+
+def test_interpolate_image_and_noise_weight():
+    # linear image interp should be perfect
+    y, x = np.mgrid[0:10, 0:10]
+    image = (10 + x*5).astype(np.float32)
+    weight = np.ones_like(image)
+    bmask = np.zeros_like(image, dtype=np.int32)
+    bad_flags = 0
+    rng = np.random.RandomState(seed=42)
+    weight[3:7, 4:9] = 0.0
+
+    # put nans here to make sure interp is done ok
+    msk = weight <= 0
+    image[msk] = np.nan
+
+    iimage, inoise = interpolate_image_and_noise(
+        image=image,
+        weight=weight,
+        bmask=bmask,
+        bad_flags=bad_flags,
+        rng=rng)
+
+    assert np.allclose(iimage, 10 + x*5)
+
+    # make sure noise field was inteprolated
+    rng = np.random.RandomState(seed=42)
+    noise = rng.normal(size=image.shape)
+    assert not np.allclose(noise[msk], inoise[msk])
+    assert np.allclose(noise[~msk], inoise[~msk])
+
+
+def test_interpolate_image_and_noise_bmask():
+    # linear image interp should be perfect
+    y, x = np.mgrid[0:10, 0:10]
+    image = (10 + x*5).astype(np.float32)
+    weight = np.ones_like(image)
+    bmask = np.zeros_like(image, dtype=np.int32)
+    bad_flags = 1
+
+    rng = np.random.RandomState(seed=42)
+    bmask[3:7, 4:9] = 1
+    bmask[:, 0] = 2
+    bmask[:, -1] = 4
+
+    # put nans here to make sure interp is done ok
+    msk = (bmask & bad_flags) != 0
+    image[msk] = np.nan
+
+    iimage, inoise = interpolate_image_and_noise(
+        image=image,
+        weight=weight,
+        bmask=bmask,
+        bad_flags=bad_flags,
+        rng=rng)
+
+    assert np.allclose(iimage, 10 + x*5)
+
+    # make sure noise field was inteprolated
+    rng = np.random.RandomState(seed=42)
+    noise = rng.normal(size=image.shape)
+    assert not np.allclose(noise[msk], inoise[msk])
+    assert np.allclose(noise[~msk], inoise[~msk])
