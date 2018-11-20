322f82b140fe6799ea7204ddb990a83239d7c42e
Matthew R Becker <becker.mr@gmail.com>
2018-11-19 20:11:41 -0600

    ENH add flagging for unmasked bleed trails

--- pizza_cutter/des_pizza_cutter/_slice_flagging.py
+++ pizza_cutter/des_pizza_cutter/_slice_flagging.py
@@ -1,6 +1,24 @@
 import numpy as np
 
 
+def compute_unmasked_trail_fraction(*, bmask):
+    """Compute the fraction of an image that has bleed trails marked outside
+    of star masks.
+
+    Parameters
+    ----------
+    bmask : array-like
+        The bit mask for the slice.
+
+    Returns
+    -------
+    frac : float
+        The fraction of the image.
+    """
+    trail_not_masked = ((bmask & 64) != 0) & ((bmask & 32) == 0)
+    return np.mean(trail_not_masked)
+
+
 def slice_full_edge_masked(*, weight, bmask, bad_flags):
     """Test if a given slice of an SE image has a full edge masked.
 
