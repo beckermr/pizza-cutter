00ffbed303330e9942b623416fec768f6dfafe92
Matthew R Becker <becker.mr@gmail.com>
2018-12-05 23:21:28 -0600

    BUG make sure interp always works

--- pizza_cutter/slice_utils/interpolate.py
+++ pizza_cutter/slice_utils/interpolate.py
@@ -1,3 +1,4 @@
+import copy
 import numpy as np
 from scipy.interpolate import CloughTocher2DInterpolator
 
@@ -16,13 +17,71 @@ def _draw_noise_image(*, weight, rng):
     return rng.normal(size=weight.shape) * np.sqrt(1.0/weight)
 
 
+def _interp_patch(*, image, bad_msk, i, j, size, buff):
+    # total patch bounds and size
+    ilow = max([i * size - buff, 0])
+    ihigh = min([ilow + size + 2*buff, image.shape[0]])
+    ni = ihigh - ilow
+
+    # bounds of the final part ot be kept (no buffer) in the final image
+    ilow_f = i * size
+    # ihigh_f = ilow_f + size  # not needed
+
+    # final part to be kept in the interpolated image
+    ilow_s = buff if ilow_f != ilow else 0
+    ihigh_s = size + ilow_s
+
+    # total patch bounds and size
+    jlow = max([j * size - buff, 0])
+    jhigh = min([jlow + size + 2*buff, image.shape[0]])
+    nj = jhigh - jlow
+
+    # bounds of the final part ot be kept in the final image
+    jlow_f = j * size
+    # jhigh_f = jlow_f + size  # not needed
+
+    # final part to be kept in the interpolated image
+    jlow_s = buff if jlow_f != jlow else 0
+    jhigh_s = size + jlow_s
+
+    npix = bad_msk[ilow:ihigh, jlow:jhigh].size
+    bm_sum = np.sum(bad_msk[ilow:ihigh, jlow:jhigh])
+    bm_frac = bm_sum / npix
+
+    if bm_frac < 0.90 and npix - bm_sum > 10:
+        # only do interpolation if the final region needs it and we have
+        # enough data
+        ii, jj = np.mgrid[0:ni, 0:nj]
+        ii = ii.ravel()
+        jj = jj.ravel()
+        imr = image[ilow:ihigh, jlow:jhigh].ravel()
+        bm = bad_msk[ilow:ihigh, jlow:jhigh].ravel()
+        gm = ~bm
+        yx = np.zeros((ii.size, 2))
+        yx[:, 0] = ii
+        yx[:, 1] = jj
+
+        # now the real work begins
+        _interp_imr = _interp_image(
+            image=imr,
+            good_msk=gm,
+            bad_msk=bm,
+            yx=yx).reshape((ni, nj))
+
+        return _interp_imr[ilow_s:ihigh_s, jlow_s:jhigh_s]
+    else:
+        # signals to caller that we need to try again with a bigger buffer
+        return None
+
+
 def _grid_interp(*, image, bad_msk):
     # this scale is a compromise between speed
     # - for images with very few pixels that require interpolation, smaller
     #    patches are better since we can skip more pixels
     # - for images that require a lot of interpolation, bigger patches are
     #   faster
-    buff = 5
+    buff_start = 5
+    delta_buff = 5
     size = 25
 
     # raise an error if the patches are not a clean multiple or the
@@ -34,57 +93,42 @@ def _grid_interp(*, image, bad_msk):
 
     interp_image = image.copy()
     for i in range(n_patches):
-        # total patch bounds and size
-        ilow = max([i * size - buff, 0])
-        ihigh = min([ilow + size + 2*buff, image.shape[0]])
-        ni = ihigh - ilow
-
         # bounds of the final part ot be kept (no buffer) in the final image
         ilow_f = i * size
         ihigh_f = ilow_f + size
 
-        # final part to be kept in the interpolated image
-        ilow_s = buff if ilow_f != ilow else 0
-        ihigh_s = size + ilow_s
-
         for j in range(n_patches):
-            # total patch bounds and size
-            jlow = max([j * size - buff, 0])
-            jhigh = min([jlow + size + 2*buff, image.shape[0]])
-            nj = jhigh - jlow
-
             # bounds of the final part ot be kept in the final image
             jlow_f = j * size
             jhigh_f = jlow_f + size
 
-            # final part to be kept in the interpolated image
-            jlow_s = buff if jlow_f != jlow else 0
-            jhigh_s = size + jlow_s
-
             if np.any(bad_msk[ilow_f:ihigh_f, jlow_f:jhigh_f]):
-                # only do interpolation if the final region needs it
-                ii, jj = np.mgrid[0:ni, 0:nj]
-                ii = ii.ravel()
-                jj = jj.ravel()
-                imr = image[ilow:ihigh, jlow:jhigh].ravel()
-                bm = bad_msk[ilow:ihigh, jlow:jhigh].ravel()
-                gm = ~bm
-                yx = np.zeros((ii.size, 2))
-                yx[:, 0] = ii
-                yx[:, 1] = jj
+                # set a flag here - if we fail the interp, then we raise
+                did_interp = False
 
-                # now the real work begins
-                _interp_imr = _interp_image(
-                    image=imr,
-                    good_msk=gm,
-                    bad_msk=bm,
-                    yx=yx).reshape((ni, nj))
+                buff = copy.copy(buff_start)
+                while buff <= size * 2:
+                    imint = _interp_patch(
+                        image=image,
+                        bad_msk=bad_msk,
+                        i=i,
+                        j=j,
+                        size=size,
+                        buff=buff)
 
-                interp_image[ilow_f:ihigh_f, jlow_f:jhigh_f] \
-                    = _interp_imr[ilow_s:ihigh_s, jlow_s:jhigh_s]
-            elif np.all(bad_msk[ilow:ihigh, jlow:jhigh]):
-                # if a whole patch is bad, then return None
-                return None
+                    if imint is None:
+                        # too few pixels, increase the buffer
+                        buff += delta_buff
+                    else:
+                        # yay - set flag, image and break out
+                        did_interp = True
+                        interp_image[ilow_f:ihigh_f, jlow_f:jhigh_f] = imint
+                        break
+
+                if not did_interp:
+                    raise RuntimeError(
+                        "Interpolation failed: buff|i|j = %d|%d|%d" % (
+                            buff, i, j))
 
     return interp_image
 
--- pizza_cutter/slice_utils/tests/test_interpolate.py
+++ pizza_cutter/slice_utils/tests/test_interpolate.py
@@ -90,3 +90,34 @@ def test_interpolate_image_and_noise_bmask(use_rng):
     noise = rng.normal(size=image.shape)
     assert not np.allclose(noise[msk], inoise[msk])
     assert np.allclose(noise[~msk], inoise[~msk])
+
+
+def test_interpolate_image_and_noise_big_missing():
+    y, x = np.mgrid[0:100, 0:100]
+    image = (10 + x*5).astype(np.float32)
+    weight = np.ones_like(image)
+    bmask = np.zeros_like(image, dtype=np.int32)
+    bad_flags = 1
+
+    rng = np.random.RandomState(seed=42)
+    bmask[15:80, 15:80] = 1
+
+    # put nans here to make sure interp is done ok
+    msk = (bmask & bad_flags) != 0
+    image[msk] = np.nan
+
+    iimage, inoise = interpolate_image_and_noise(
+        image=image,
+        weight=weight,
+        bmask=bmask,
+        bad_flags=bad_flags,
+        rng=rng)
+
+    # interp will be waaay off but shpuld have happened
+    assert np.all(np.isfinite(iimage))
+
+    # make sure noise field was inteprolated
+    rng = np.random.RandomState(seed=42)
+    noise = rng.normal(size=image.shape)
+    assert not np.allclose(noise[msk], inoise[msk])
+    assert np.allclose(noise[~msk], inoise[~msk])
