044c3c893f36e53e44d46696a34470728b8a885e
Matthew R Becker <becker.mr@gmail.com>
2019-05-14 14:53:11 -0500

    added affine WCS classes

---
+++ pizza_cutter/des_pizza_cutter/_affine_wcs.py
@@ -0,0 +1,118 @@
+
+
+class AffineWCS(object):
+    """An affine WCS class which uses the esutil.wcsutil API.
+
+    This class defines a mapping from image coordinates (x, y) to
+    coordinates (u, v) that is defined by
+
+        u = dudx * (x - x0) + dudy * (y - y0)
+        v = dvdx * (x - x0) + dvdy * (y - y0)
+
+    Note that (u,v) are traditionally in units of arcsec.
+
+    Parameters
+    ----------
+    dudx : float
+        The derivative of u wrt x for the Jacobian.
+    dudy : float
+        The derivative of u wrt y for the Jacobian.
+    dvdx : float
+        The derivative of v wrt x for the Jacobian.
+    dvdy : float
+        The derivative of v wrt y for the Jacobian.
+    x0 : float
+        The x/column part of the location of (u, v) = (0, 0) in pixel
+        coordinates.
+    y0 : float
+        The y/row part of the location of (u, v) = (0, 0) in pixel
+        coordinates.
+
+    Methods
+    -------
+    image2sky(x, y)
+        Convert image coordinates to sky coordinates.
+    sky2image(u, v)
+        Convert sky coordinates to image coordinates.
+    get_jacobian(x, y)
+        Get the local Jacobian of the affine transformation.
+
+    Attributes
+    ----------
+    Any input parameter is also an attribute.
+    """
+    def __init__(self, dudx, dudy, dvdx, dvdy, x0, y0):
+        self.dudx = dudx
+        self.dudy = dudy
+        self.dvdx = dvdx
+        self.dvdy = dvdy
+        self.x0 = x0
+        self.y0 = y0
+
+        self._det = dudx * dvdy - dvdx * dudy
+        if self._det == 0:
+            raise ValueError("Affine transformation is not invertible!")
+
+    def image2sky(self, x, y):
+        """Convert image coordinates to sky coordinates.
+
+        Parameters
+        ----------
+        x : float or array-like
+            The x/column coordinate usually in pixels.
+        y : float or array-like
+            The y/row coordinate in usually pixels.
+
+        Returns
+        -------
+        u : float or array-like
+            The u-coordinate usually in arcsec.
+        v : float or array-like
+            The v-coordinate usually in arcsec.
+        """
+        u = self.dudx * (x - self.x0) + self.dudy * (y - self.y0)
+        v = self.dvdx * (x - self.x0) + self.dvdy * (y - self.y0)
+        return u, v
+
+    def sky2image(self, u, v):
+        """Convert sky coordinates to image coordinates.
+
+        Parameters
+        -------
+        u : float or array-like
+            The u-coordinate usually in arcsec.
+        v : float or array-like
+            The v-coordinate usually in arcsec.
+
+        Returns
+        ----------
+        x : float or array-like
+            The x/column coordinate usually in pixels.
+        y : float or array-like
+            The y/row coordinate in usually pixels.
+        """
+        x = (self.dvdy * u - self.dudy * v) / self._det + self.x0
+        y = (-self.dvdx * u + self.dudx * v) / self._det + self.y0
+        return x, y
+
+    def get_jacobian(self, x, y):
+        """Get the local Jacobian of the affine transformation.
+
+        Parameters
+        ----------
+        x : float or array-like
+            Ignored. Present for API compatibility.
+        y : float or array-like
+            Ignored. Present for API compatibility.
+
+        Returns
+        -------
+        jac : tuple of floats
+            A tuple of the values (dudx, dudy, dvdx, dvdy).
+        """
+        return (
+            self.dudx,
+            self.dudy,
+            self.dvdx,
+            self.dvdy
+        )
---
+++ pizza_cutter/des_pizza_cutter/tests/test_affine_wcs.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+import pytest
+
+from .._affine_wcs import AffineWCS
+
+
+def test_affine_wcs_values():
+    wcs = AffineWCS(
+        dudx=0.5,
+        dudy=0.7,
+        dvdx=-0.1,
+        dvdy=-2.5,
+        x0=56,
+        y0=-1345
+    )
+    u, v = wcs.image2sky(0.4, 0.8)
+    assert u == 0.5 * (0.4 - 56) + 0.7 * (0.8 + 1345)
+    assert v == -0.1 * (0.4 - 56) - 2.5 * (0.8 + 1345)
+
+
+def test_affine_wcs_jacobian():
+    wcs = AffineWCS(
+        dudx=0.54,
+        dudy=0.384257,
+        dvdx=-0.3209411,
+        dvdy=-3913431,
+        x0=0,
+        y0=0
+    )
+    jac = wcs.get_jacobian(None, None)
+    assert jac[0] == wcs.dudx
+    assert jac[1] == wcs.dudy
+    assert jac[2] == wcs.dvdx
+    assert jac[3] == wcs.dvdy
+
+
+def test_affine_wcs_inverse():
+    rng = np.random.RandomState(seed=19)
+    for _ in range(10):
+        wcs = AffineWCS(
+            dudx=rng.uniform()-0.5,
+            dudy=rng.uniform()-0.5,
+            dvdx=rng.uniform()-0.5,
+            dvdy=rng.uniform()-0.5,
+            x0=rng.uniform()-0.5,
+            y0=rng.uniform()-0.5
+        )
+
+        x, y = rng.normal(size=10), rng.normal(size=10)
+        u, v = wcs.image2sky(x, y)
+        _x, _y = wcs.sky2image(u, v)
+
+        assert np.allclose(x, _x)
+        assert np.allclose(y, _y)
+
+
+def test_affine_wcs_zero_det_raises():
+    with pytest.raises(ValueError):
+        AffineWCS(
+            dudx=0,
+            dudy=0,
+            dvdx=0,
+            dvdy=0,
+            x0=0,
+            y0=0)
