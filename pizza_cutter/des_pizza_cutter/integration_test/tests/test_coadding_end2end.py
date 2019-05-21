2b30924d8759084e46884635ccfb14f4a93b8c21
Matthew R Becker <becker.mr@gmail.com>
2019-05-20 19:29:55 -0500

    set some benchmarks

--- pizza_cutter/des_pizza_cutter/integration_test/tests/test_coadding_end2end.py
+++ pizza_cutter/des_pizza_cutter/integration_test/tests/test_coadding_end2end.py
@@ -265,11 +265,71 @@ def test_coadding_end2end_psf(coadd_end2end):
         nx=51, ny=51, scale=0.25).array
 
     # we don't have a strict criterion here since the coadding process
-    # broadens the PSF a bit. Instead we are setting a tolerance and it code
-    # changes make this break, then we need to look again.
+    # broadens the PSF a bit. Instead we are setting tolerances and if code
+    # changes make these break, then we need to look again.
     assert np.max(np.abs(psf_im - psf_m))/np.max(psf_im) < 0.003
 
     # we also demand that the FWHM is about the same
     assert np.abs(
         galsim.ImageD(psf_im, scale=0.25).calculateFWHM() -
         galsim.ImageD(psf_m, scale=0.25).calculateFWHM()) < 1e-4
+
+
+def test_coadding_end2end_gal(coadd_end2end):
+    m = meds.MEDS(coadd_end2end['meds_path'])
+    weights = coadd_end2end['weights']
+    info = coadd_end2end['info']
+    ei = m._fits['epochs_info'].read()
+    gal = galsim.Gaussian(fwhm=0.5).shear(g1=0, g2=0.5)
+
+    ############################################################
+    # get weights for computing nepoch_eff
+    max_wgts = []
+    psfs = []
+    for ind in range(len(ei)):
+        if ei['weight'][ind] > 0:
+            src_ind = ei['file_id'][ind]-1
+            max_wgts.append(
+                np.max(weights[src_ind]) /
+                info['src_info'][src_ind]['scale'] ** 2
+                )
+            psfs.append(
+                galsim.Gaussian(
+                    fwhm=info['src_info'][src_ind]['galsim_psf_config']['fwhm']
+                )
+            )
+    max_wgts = np.array(max_wgts)
+    max_wgts = max_wgts / np.sum(max_wgts)
+
+    gal_m = m.get_cutout(0, 0, type='image')
+
+    gal_psf = [
+        galsim.Convolve(gal, psf.withFlux(wgt))
+        for psf, wgt in zip(psfs, max_wgts)]
+    gal_im = galsim.Sum(gal_psf).withFlux(np.sum(gal_m)).drawImage(
+        nx=49, ny=49, scale=0.25).array
+
+    # we don't have a strict criterion here since the coadding process
+    # broadens the PSF a bit. Instead we are setting tolerances and if code
+    # changes make these break, then we need to look again.
+    assert np.max(np.abs(gal_im - gal_m))/np.max(gal_im) < 0.03
+
+    # we also demand that the FWHM is about the same
+    assert np.abs(
+        galsim.ImageD(gal_im, scale=0.25).calculateFWHM() -
+        galsim.ImageD(gal_m, scale=0.25).calculateFWHM()) < 1e-2
+
+    # and the center
+    y, x = np.mgrid[0:49, 0:49]
+    assert np.abs(
+        np.sum(x*gal_m)/np.sum(gal_m) -
+        np.sum(x*gal_im)/np.sum(gal_m)) < 1e-2
+    assert np.abs(
+        np.sum(y*gal_m)/np.sum(gal_m) -
+        np.sum(y*gal_im)/np.sum(gal_m)) < 1e-2
+
+    # and shear
+    mom_im = galsim.hsm.FindAdaptiveMom(galsim.ImageD(gal_im, scale=0.25))
+    mom_m = galsim.hsm.FindAdaptiveMom(galsim.ImageD(gal_m, scale=0.25))
+    assert np.abs(mom_im.observed_shape.e1 - mom_m.observed_shape.e1) < 0.005
+    assert np.abs(mom_im.observed_shape.e2 - mom_m.observed_shape.e2) < 0.005
