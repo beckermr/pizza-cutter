8f24b735a8066efd3d7b39fbc637623b91d2cdd3
Matthew R. Becker <becker.mr@gmail.com>
2021-04-05 09:27:07 -0500

    remove old flag

--- pizza_cutter/slice_utils/procflags.py
+++ pizza_cutter/slice_utils/procflags.py
@@ -7,8 +7,5 @@ SLICE_HAS_FLAGS = 2**1
 # high masked frac
 HIGH_MASKED_FRAC = 2**2
 
-# too much trail not masked by bright star mask
-HIGH_UNMASKED_TRAIL_FRAC = 2**3
-
 # second check of masking in interp
 HIGH_INTERP_MASKED_FRAC = 2**4
