841f2b87b48c33f72574349193aef65550d1746f
Matthew R. Becker <beckermr@users.noreply.github.com>
2021-04-06 08:31:59 -0500

    Update pizza_cutter/des_pizza_cutter/_coadd_slices.py

--- pizza_cutter/des_pizza_cutter/_coadd_slices.py
+++ pizza_cutter/des_pizza_cutter/_coadd_slices.py
@@ -592,7 +592,6 @@ def _coadd_slice_inputs(
 
     # fpacking will produce negative outputs on the way out so we truncate the
     # masked fraction to zero if it is less than 1e-3 to help eliminate this
-    # we also do the same for values close to 1
     msk = (np.isclose(interp_se_frac, 0, rtol=1e-3, atol=1e-3) & (interp_se_frac != 0))
     if np.any(msk):
         interp_se_frac[msk] = 0
