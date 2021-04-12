d389d377ab35e3556c0d9e00c40d08dcdfb935c1
Matthew R. Becker <becker.mr@gmail.com>
2021-04-12 09:45:23 -0500

    ENH add ability to rotate by degrees

--- pizza_cutter/slice_utils/symmetrize.py
+++ pizza_cutter/slice_utils/symmetrize.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.ndimage
 
 
 def symmetrize_weight(*, weight):
@@ -20,7 +21,7 @@ def symmetrize_weight(*, weight):
         weight[msk] = 0.0
 
 
-def symmetrize_bmask(*, bmask):
+def symmetrize_bmask(*, bmask, angle=None):
     """Symmetrize masked pixels.
 
     WARNING: This function operates in-place!
@@ -29,8 +30,28 @@ def symmetrize_bmask(*, bmask):
     ----------
     bmask : array-like
         The bit mask for the slice.
+    angle : float
+        If not None, then the routine `scipy.ndimage.rotate` is used for the mask
+        symmetrization with this angle in degrees. Note that specifying 90 degress
+        will not produce the same output as specifying None.
     """
     if bmask.shape[0] != bmask.shape[1]:
         raise ValueError("Only square images can be symmetrized!")
 
-    bmask |= np.rot90(bmask)
+    if angle is None:
+        bmask |= np.rot90(bmask)
+    else:
+        for i in range(32):
+            bit = 2**i
+            msk = (bmask & bit) != 0
+            if np.any(msk):
+                mask_for_bit = np.zeros_like(bmask, dtype=np.float32)
+                mask_for_bit[msk] = 1.0
+                mask_for_bit = scipy.ndimage.rotate(
+                    mask_for_bit,
+                    angle,
+                    reshape=False,
+                    order=1,
+                )
+                msk = mask_for_bit > 0
+                bmask[msk] |= bit
--- pizza_cutter/slice_utils/tests/test_symmetrize.py
+++ pizza_cutter/slice_utils/tests/test_symmetrize.py
@@ -26,3 +26,33 @@ def test_symmetrize_bmask():
          [3, 2, 2, 2],
          [1, 0, 2, 0],
          [1, 1, 3, 1]])
+
+
+def test_symmetrize_bmask_angle():
+    bmask = np.zeros((64, 64), dtype=np.int32)
+    bmask[:, 0] = 1
+    bmask[:, -2] = 2
+    bmask_orig = bmask.copy()
+    bmasks = []
+    for angle in [45, 90, 135, 180, 225, 270, 315]:
+        bmask_r = bmask_orig.copy()
+        symmetrize_bmask(bmask=bmask_r, angle=angle)
+        bmasks.append(bmask_r)
+        bmask |= bmask_r
+
+    assert np.array_equal(bmask, np.rot90(bmask))
+    assert np.array_equal(bmask, np.rot90(np.rot90(bmask)))
+    assert np.array_equal(bmask, np.rot90(np.rot90(np.rot90(bmask))))
+
+    if False:
+        import matplotlib.pyplot as plt
+        fig, axs = plt.subplots(nrows=3, ncols=3)
+        axs = axs.ravel()
+        bmasks.append(bmask_orig)
+        bmasks.append(bmask)
+        for i, bm in enumerate(bmasks):
+            ax = axs[i]
+            ax.pcolormesh(bm)
+            ax.set_aspect(1)
+        import pdb
+        pdb.set_trace()
