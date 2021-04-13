71a73ffc1638636b814f56f76c46af8e8e098f24
Erin Sheldon <erin.sheldon@gmail.com>
2021-04-13 11:50:52 -0400

    import the constants.  These are useful downstream

--- pizza_cutter/des_pizza_cutter/__init__.py
+++ pizza_cutter/des_pizza_cutter/__init__.py
@@ -1,4 +1,5 @@
-from ._pizza_cutter import make_des_pizza_slices  # noqa
-from ._des_info import add_extra_des_coadd_tile_info  # noqa
-from ._constants import BMASK_SPLINE_INTERP, BMASK_NOISE_INTERP  # noqa
-from ._load_info import load_objects_into_info  # noqa
+# flake8: noqa
+from ._pizza_cutter import make_des_pizza_slices
+from ._des_info import add_extra_des_coadd_tile_info
+from ._constants import *
+from ._load_info import load_objects_into_info
