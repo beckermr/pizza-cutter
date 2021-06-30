3af363e268123c3c46dc889a539bb090232fe4dd
Matthew R. Becker <becker.mr@gmail.com>
2021-06-30 06:02:59 -0500

    ENH refactor interp

--- pizza_cutter/slice_utils/tests/test_interpolate.py
+++ pizza_cutter/slice_utils/tests/test_interpolate.py
@@ -5,7 +5,26 @@ import pytest
 from ..interpolate import (
     interpolate_image_and_noise,
     copy_masked_edges_image_and_noise,
+    interpolate_image_at_mask,
 )
+
+
+def test_interpolate_image_at_mask():
+    # linear image interp should be perfect for regions smaller than the
+    # patches used for interpolation
+    y, x = np.mgrid[0:100, 0:100]
+    image = (10 + x*5).astype(np.float32)
+    bmask = np.zeros_like(image, dtype=bool)
+    bmask[30:35, 40:45] = True
+
+    # put nans here to make sure interp is done ok
+    image[bmask] = np.nan
+
+    iimage = interpolate_image_at_mask(
+        image=image,
+        bad_msk=bmask,
+    )
+    assert np.allclose(iimage, 10 + x*5)
 
 
 def test_interpolate_image_and_noise_weight():
