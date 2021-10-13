29150f6fe669612f1e2aaed790f0f523db2dbeee
Matthew R. Becker <becker.mr@gmail.com>
2021-10-13 16:38:06 -0500

    REF load the WCs properly

--- pizza_cutter/des_pizza_cutter/_load_info.py
+++ pizza_cutter/des_pizza_cutter/_load_info.py
@@ -155,11 +155,14 @@ def load_objects_into_info(*, info, verbose=True, skip_se=False):
                 piff_path = expandvars(ii['piff_path'])
                 try:
                     ii['piff_psf'] = get_piff_psf(piff_path)
-                    ii['piff_wcs'] = ii['piff_psf'].wcs[0]
+                    if isinstance(ii['piff_psf'].wcs, list):
+                        ii['piff_wcs'] = ii['piff_psf'].wcs[0]
+                    else:
+                        ii['piff_wcs'] = ii['piff_psf'].wcs[ii["ccdnum"]]
 
                     # try and grab pixmappy from piff
-                    if isinstance(ii['piff_psf'].wcs[0], pixmappy.GalSimWCS):
-                        ii['pixmappy_wcs'] = ii['piff_psf'].wcs[0]
+                    if isinstance(ii['piff_wcs'], pixmappy.GalSimWCS):
+                        ii['pixmappy_wcs'] = ii['piff_wcs']
 
                         # HACK at the internals to code around a bug!
                         if isinstance(
