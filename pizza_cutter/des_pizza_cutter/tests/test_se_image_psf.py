3b444fa132e705fa311ce261d027dee28d6d1614
conda-forge-admin <pelson.pub+conda-forge@gmail.com>
2022-06-16 11:31:47 -0500

    DOC add license

--- pizza_cutter/des_pizza_cutter/tests/test_se_image_psf.py
+++ pizza_cutter/des_pizza_cutter/tests/test_se_image_psf.py
@@ -369,6 +369,31 @@ def test_se_image_psf_piff_hsm(se_image_data):
 
         res = galsim.hsm.FindAdaptiveMom(im)
         # this code is lifted from Piff under its license
+        # Copyright (c) 2016 by Mike Jarvis and the other collaborators on GitHub at
+        # https://github.com/rmjarvis/Piff  All rights reserved.
+        #
+        # Piff is free software: Redistribution and use in source and binary forms,
+        # with or without modification, are permitted provided that the following
+        # conditions are met:
+        #
+        # 1. Redistributions of source code must retain the above copyright notice, this
+        #    list of conditions and the following disclaimer.
+        #
+        # 2. Redistributions in binary form must reproduce the above copyright notice,
+        #    this list of conditions and the following disclaimer in the documentation
+        #    and/or other materials provided with the distribution.
+        #
+        # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+        # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+        # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+        # DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+        # FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+        # DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+        # SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+        # CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+        # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+        # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+        # start of lifted code from Piff
         sigma = res.moments_sigma
         shape = res.observed_shape
         scale, shear, theta, flip = jac.getDecomposition()
@@ -381,6 +406,7 @@ def test_se_image_psf_piff_hsm(se_image_data):
         shape = galsim.Shear(g=shape.g, beta=shape.beta + theta)
         # Finally the shear
         shape = shear + shape
+        # enf of lifted code from Piff
 
         # this T is actually sigma due to a Piff bug
         assert np.allclose(sigma, hsm_cat["T_model"][i], atol=5e-4, rtol=0)
